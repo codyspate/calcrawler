cd26c25e82dd92b9ed3d0c00577e7ea3388e6c75
Spate <Cody.Spate@state.ca.gov>
2016-12-12 12:09:55 -0800

    initial commit

---
+++ general.py
@@ -0,0 +1,65 @@
+import os, shutil
+
+
+# Each website is a separate project (folder)
+def create_project_dir(directory):
+    if not os.path.exists(directory):
+        print('Creating directory ' + directory)
+        os.makedirs(directory)
+
+
+# Create queue and crawled files (if not created)
+def create_data_files(project_name, base_url):
+    queue = os.path.join('projects', os.path.join(project_name , 'queue.txt'))
+    crawled = os.path.join('projects', os.path.join(project_name,"crawled.txt"))
+    summary = os.path.join('projects', os.path.join(project_name, "summary.txt"))
+    delete_file_contents(queue)
+    delete_file_contents(crawled)
+    delete_file_contents(summary)
+    if not os.path.isfile(queue):
+        write_file(queue, base_url)
+    if not os.path.isfile(crawled):
+        write_file(crawled, '')
+    if not os.path.isfile(summary):
+        t = "PDF count:     \nHTML/HTM count:     \nMedia files:     \nOther:     \nErrors:     \n\nTotal size:     "
+        write_file(summary, t)
+
+
+# Create a new file
+def write_file(path, data):
+    with open(path, 'w') as f:
+        f.write(data)
+
+
+# Add data onto an existing file
+def append_to_file(path, data):
+    with open(path, 'a') as file:
+        file.write(data + '\n')
+
+
+# Delete the contents of a file
+def delete_file_contents(path):
+    open(path, 'w').close()
+
+def update_summary(path, pname, url, pdf, html, media, other, errors, pages, size):
+    delete_file_contents(path)
+    with open(path, 'a') as f:
+        f.write("Cal Crawler v1.0\n\n")
+        f.write("Website: " + pname + '\n')
+        f.write("URL: "+ url + '\n\n')
+        f.write("PDF count: " + pdf + "\nHTML/HTM count: " + html + "\nMedia files: " + media + "\nOther: " + other + "\nErrors: " + errors + "\n\nTotal Number of Pages: " + pages + "\nTotal size: " + size  + " MB")
+
+# Read a file and convert each line to set items
+def file_to_set(file_name):
+    results = set()
+    with open(file_name, 'rt') as f:
+        for line in f:
+            results.add(line.replace('\n', ''))
+    return results
+
+
+# Iterate through a set, each item will be a line in a file
+def set_to_file(links, file_name):
+    with open(file_name,"w") as f:
+        for l in sorted(links):
+            f.write(l+"\n")
---
+++ spider.py
@@ -0,0 +1,102 @@
+from urllib.request import urlopen
+from link_finder import LinkFinder
+from domain import *
+from general import *
+
+
+class Spider:
+
+    project_name = ''
+    base_url = ''
+    domain_name = ''
+    queue_file = ''
+    crawled_file = ''
+    summary_file = ''
+    num_pdf = 0
+    num_html = 0
+    num_media = 0
+    num_other = 0
+    num_errors = 0
+    total_size = 0
+    pages = 0
+    queue = set()
+    crawled = set()
+
+    def __init__(self, project_name, base_url, domain_name):
+        Spider.project_name = project_name
+        Spider.base_url = base_url
+        Spider.domain_name = domain_name
+        Spider.queue_file = 'projects/' + Spider.project_name + '/queue.txt'
+        Spider.crawled_file = 'projects/' + Spider.project_name + '/crawled.txt'
+        Spider.summary_file = 'projects/' + Spider.project_name + '/summary.txt'
+        self.boot()
+        self.crawl_page('First spider', Spider.base_url)
+
+    # Creates directory and files for project on first run and starts the spider
+    @staticmethod
+    def boot():
+        create_project_dir("projects/" + Spider.project_name)
+        create_data_files(Spider.project_name, Spider.base_url)
+        Spider.queue = file_to_set(Spider.queue_file)
+        Spider.crawled = file_to_set(Spider.crawled_file)
+
+    # Updates user display, fills queue and updates files
+    @staticmethod
+    def crawl_page(thread_name, page_url):
+        if page_url not in Spider.crawled:
+            print(thread_name + ' now crawling ' + page_url)
+            print('Queue ' + str(len(Spider.queue)) + ' | Crawled  ' + str(len(Spider.crawled)))
+            links = Spider.gather_links(page_url)
+            Spider.add_links_to_queue(links)
+            Spider.queue.remove(page_url)
+            Spider.crawled.add(page_url)
+            Spider.update_files()
+
+    # Converts raw response data into readable information and checks for proper html formatting
+    @staticmethod
+    def gather_links(page_url):
+        Spider.pages += 1
+        html_string = ''
+        try:
+            response = urlopen(page_url)
+            if 'text/html' in response.getheader('Content-Type'):
+                html_bytes = response.read()
+                Spider.total_size += (len(html_bytes)/1000)
+                html_string = html_bytes.decode("utf-8")
+            finder = LinkFinder(Spider.base_url, page_url)
+            finder.feed(html_string)
+            if ".pdf" in page_url.lower():
+                Spider.num_pdf += 1
+            media_types = ['.mp3', '.jpg', '.png', '.mpeg', '.ico', '.wmv', '.avi', '.mov', '.mng', '.gif', '.bmp', '.jpeg']
+            for t in media_types:
+                if t in page_url.lower():
+                    Spider.num_media += 1
+                    break
+            html_types = ['.html', '.htm', '.xhtml', '.asp', '.aspx', '.php', '.mhtml']
+            for t in html_types:
+                if t in page_url.lower() or (not page_url[-4] == '.' and not page_url[-5] == '.'):
+                    Spider.num_html += 1
+                    break
+
+        except Exception as e:
+            print(str(e))
+            Spider.num_errors += 1
+            return set()
+        return finder.page_links()
+
+    # Saves queue data to project files
+    @staticmethod
+    def add_links_to_queue(links):
+        for url in links:
+            if (url in Spider.queue) or (url in Spider.crawled):
+                continue
+            if not Spider.domain_name in get_domain_name(url):
+                continue
+            Spider.queue.add(url)
+
+    @staticmethod
+    def update_files():
+        set_to_file(Spider.queue, Spider.queue_file)
+        set_to_file(Spider.crawled, Spider.crawled_file)
+        size = Spider.total_size/1000
+        update_summary(Spider.summary_file, Spider.project_name, Spider.base_url, str(Spider.num_pdf), str(Spider.num_html), str(Spider.num_media), str(Spider.num_other), str(Spider.num_errors), str(Spider.pages), str(size))
